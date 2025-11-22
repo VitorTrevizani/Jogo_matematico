2e416312472bae2fc0ec38996ef6b858034f170a
VitorTrevizani <vitoralextrevizani6@gmail.com>
2025-11-22 08:37:45 -0300

    início do desenvolvimento da página home.html
    Foram adicionados elementos como o nome do jogo e menu de navegação, além da possibilidade de escolher o modo de jogo(ranqueado ou personalizado) e a dificuldade.

---
+++ app/templates/script.js
@@ -0,0 +1,5 @@
+const menu = document.getElementById("ranqueado");
+menu.addEventListener("click", () =>{
+    document.getElementById("escolha_modo").style.display = "none";
+    document.getElementById("escolha_dificuldade").style.display = "grid";
+})
