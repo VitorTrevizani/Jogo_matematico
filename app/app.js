6dbe54e45321013b40cb047b94824103a08f1a25
VitorTrevizani <vitoralextrevizani6@gmail.com>
2025-12-20 16:05:02 -0300

    erros, login e melhorias na segurança
    adicionei:
    1- o flash para que mensagens de erro sejam exibidas ao usuário;
    2- cadastro e login de usuário,
    3-  função Hash para garantir a segurança das senhas no banco de dados;
    4- validação de formulário

--- app/app.js
+++ app/app.js
@@ -5,12 +5,15 @@ const { engine } = require("express-handlebars")
 const mongoose = require("mongoose")
 const path = require("path")
 const game = require("./routes/game")
+const session = require("express-session")
+const flash = require("connect-flash")
 
 //configurações
     //public
     app.use(express.static(path.join(__dirname,"public"))) //transforma a pasta public em raiz publica, assim nao éprecisso especificar ela no html
    //body-parser
-        //
+        app.use(bodyParser.urlencoded({extended:true}))  
+        app.use(bodyParser.json())
    //mongoose
        mongoose.connect("mongodb://localhost:27017/mathApp").then(() => {
         console.log("Mongoose conectado!")
@@ -22,9 +25,21 @@ const game = require("./routes/game")
     app.set("view engine", "handlebars")
 
    //session
-       //
+    app.use(session({
+        secret: "math123vitor",
+        resave: true,
+        saveUninitialized: true
+    }))
+
+    //flash
+    app.use(flash())
+
    //middleware
-       //
+    app.use((req, res, next) => {
+        res.locals.success_msg = req.flash("success_msg")
+        res.locals.error_msg = req.flash("error_msg")
+        next()
+    })
    //rotas
    app.use("/game", game)
         
--- app/routes/game.js
+++ app/routes/game.js
@@ -3,25 +3,106 @@ const router  = express.Router()
 const mongoose = require("mongoose")
 require("../models/Usuario")
 const Usuario = mongoose.model("usuarios")
+const bcrypt = require("bcryptjs")
 
 router.get("/", (req, res) => {
     res.render("home")
 })
 
-// router.get("/login", (req, res) => {
+router.get("/login", (req, res) => {
+    res.render("login")
+})
 
-// })
+router.post("/login/fazerlogin", (req, res) => {
+    Usuario.findOne({nome: req.body.name, senha: req.body.password}).lean().then((usuario) => {
+        if(!usuario){
+            req.flash("error_msg", "usuário não encontrado")
+            res.redirect("/game/login")
+        }
+        else{
+            req.flash("success_msg", "Usuário logado com sucesso")
+            res.redirect("/game")
+        }
+        
+    }).catch((err) => {
+        req.flash("error_msg", "Falha ao fazer login")
+        res.redirect("/game/login")
+    })
+})
 
-// router.post("/login/fazerlogin", (req, res) => {
+router.get("/cadastro", (req, res) => {
+    res.render("cadastro")
+})
 
-// })
 
-// router.get("/cadastro", (req, res) => {
 
-// })
 
-// router.get("/cadastro/fazercadastro" (req, res) => {
+router.post("/cadastro/fazercadastro", (req, res) => {
 
-// })
+    // validação: 
+
+    let erros = []
+
+    if(req.body.name == null || typeof req.body.name == "undefined" || req.body.password == null || typeof req.body.password == "undefined"){
+        erros.push({texto:"campos vazios não são permitidos"})
+    }
+
+    if(req.body.name.length < 5 || req.body.name.length > 15){
+        erros.push({texto:"O nome deve ter 5 a 15 caracteres"})
+    }
+
+    if(req.body.password.length < 5 || req.body.password.length > 10 ){
+        erros.push({texto:"A senha deve ter de 5 a 10 caracteres"})
+
+    }
+    
+       
+    if(erros.length > 0){
+        res.render("cadastro", {erros: erros})
+    }else{
+
+        const newUser = {
+            nome: req.body.name,
+            senha: req.body.password
+        }
+
+        //procura pra ver se já existe
+        Usuario.findOne({nome: newUser.nome, senha: newUser.senha}).lean().then((usuario) => {
+            if(usuario){
+                req.flash("error_msg","Já existe um usuário com esse nome e senha cadastrados")
+                res.redirect("/game/cadastro")
+            }
+            else{
+            //adiciona no banco caso aonda não exista
+                bcrypt.genSalt(10, (erro, salt) => {
+                    bcrypt.hash(newUser.senha, salt, (erro, hash) => {
+                        if(erro){
+                            req.flash("error_msg", "houve um erro durante o salvamento do usuario")
+                            res.redirect("/game")
+                        }
+                        newUser.senha = hash
+                        new Usuario (newUser).save().then(() => {
+                            req.flash("success_msg", "usuário cadastrado com sucesso")
+                            res.redirect("/game")
+                        }).catch((err) => {
+                            req.flash("error_msg", "Falha ao cadastrar usuário")
+                            res.redirect("/game/cadastro")
+                })
+                    })
+                })
+
+                  
+            }
+           
+        }).catch((err) => {
+            req.flash("error_msg", "falha ao cadastrar usuário")
+            res.redirect("/game/cadastro")
+        })
+    }
+
+    
+})
+
+   
 
 module.exports = router
